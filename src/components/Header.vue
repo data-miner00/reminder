85717e2a9ce02eb5f3d6ce8c2086ca7a859ba0f4
LeongXianJun <leongxianjun@1utar.my>
2020-09-24 12:32:13 +0800

    Header Improvement

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,63 +1,78 @@
 <template>
-  <header>
-		<div class="logo">Reminder</div>
-		<input type="text" id="search" />
-		<div class="bind-one">
-			<div class="user">
-				<img 
-					src="https://img.icons8.com/plasticine/2x/google-logo.png" 
-					width="30" 
-					height="30" 
-					class="avatar" 
-				/>
-				<div class="username">{{ username }}</div>
-			</div>
-			<div class="logout">Logout</div>
-		</div>
-	</header>
+  <header class="md-layout">
+    <div class="md-layout-item md-xlarge-size-20 md-large-size-10 md-medium-hide"></div>
+    <div class="md-layout-item md-xlarge-size-60 md-large-size-80 md-medium-size-100 content">
+      <div class="left">
+        <span class="md-display-1 logo">Reminder</span>
+      </div>
+      <div class="center">
+        <input type="text" id="search" />
+      </div>
+      <div class="right">
+        <md-avatar>
+          <img src="https://img.icons8.com/plasticine/2x/google-logo.png" alt="avatar" />
+        </md-avatar>
+        <span class="md-body-1 md-layout-item md-small-hide">{{username}}</span>
+        <md-button to="login" class="md-body-1 logout">Logout</md-button>
+      </div>
+    </div>
+    <div class="md-layout-item md-xlarge-size-20 md-large-size-10 md-medium-hide"></div>
+  </header>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			username: 'Arkadian',
-		}
-	}
-}
+  data() {
+    return {
+      username: "Arkadian",
+    };
+  },
+};
 </script>
 
 <style lang="sass" scoped>
 header
-	display: flex
-	flex-direction: row
-	height: 60px
+	position: fixed
 	width: 100%
+	display: flex
+	flex: 1
+	padding-top: 5px
+	padding-bottom: 5px
 	background-color: #1B1725
-	justify-content: space-around
+
+.content
+	display: flex
+	flex: 1
+	justify-content: space-between
+
+.left
+	display: flex
+	flex: 1
 	align-items: center
 
-	.logo
-		text-transform: uppercase
-		letter-spacing: 2px
-		font-size: 2em
-		color: white
+.logo
+	text-transform: uppercase
+	color: white
 
-	#search
-		width: 30%
-		height: 35px
+.center
+	display: flex
+	flex: 3
+	justify-content: center
+	align-items: center
 
-	.bind-one
-		height: 35px
-		width: 13%
-		display: flex
-		justify-content: space-around
-		align-items: center
-		color: white
-		.user
-			display: flex
-			align-items: center
-			justify-content: space-between
-			.username
-				font-weight: bold
+#search
+	width: 80%
+	height: 35px
+	padding-left: 10px
+	padding-right: 10px
+	border-radius: 5px
+
+.right
+	display: flex
+	flex: 1
+	align-items: center
+	color: white
+
+.logout
+	color: white
 </style>
